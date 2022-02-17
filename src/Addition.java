abeb14da56466538d371b8d7055ef1b48a30b79c
RailYunusov <opel808437@gmail.com>
2022-02-17 20:27:57 +0300

    initial commit

---
+++ src/Addition.java
@@ -0,0 +1,21 @@
+/**
+ * Класс с логикой математических операций
+ */
+public class Addition {
+
+    public Double plus( Double val1, Double val2){
+        return val1 + val2;
+    }
+    public Double minus( Double val1, Double val2){
+        return val1 - val2;
+    }
+    public Double multiply( Double val1, Double val2){
+        return val1 * val2;
+    }
+
+    public Double div( Double val1, Double val2){
+        return val1 / val2;
+    }
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+/**
+ * Главный класс
+ */
+public class Main {
+    public static void main(String[] args) {
+        Addition ad = new Addition();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Добро пожаловать в калькулятор.");
+        while (true) {
+            System.out.println("Введите первое число.");
+            Double first = scanner.nextDouble();
+            System.out.println("Теперь введите оператор: + сложение, - вычитание, * умножение, / деление");
+            String operator = scanner.next();
+            if ("exit".equalsIgnoreCase(operator)){
+                scanner.close();
+                return;
+            }
+            System.out.println("Теперь введите второе число.");
+            Double two = scanner.nextDouble();
+            if ("+".equals(operator)) {
+                System.out.printf("Ответ: %.4f\n", ad.plus(first, two));
+            }
+            if ("-".equals(operator)) {
+                System.out.printf("Ответ: %.4f\n", ad.minus(first, two));
+            }
+            if ("*".equals(operator)) {
+                System.out.printf("Ответ: %.4f\n", ad.multiply(first, two));
+            }
+            if ("/".equals(operator)) {
+                if (two == 0) {
+                    System.err.println("На 0 делить нельзя!");
+                    scanner.close();
+                    return;
+                }
+                System.out.printf("Ответ: %.4f\n", ad.div(first, two));
+            }
+            System.out.println("Для выхода из калькулятора вместо оператора введите EXIT");
+        }
+    }
+}
