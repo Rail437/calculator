b19278596863ef64353a4ba106223e0020fb47ce
RailYunusov <opel808437@gmail.com>
2022-02-17 20:31:56 +0300

    v2

--- src/Addition.java
+++ src/Addition.java
@@ -3,16 +3,42 @@
  */
 public class Addition {
 
+    /**
+     * Метод сложения чисел
+     * @param val1
+     * @param val2
+     * @return
+     */
     public Double plus( Double val1, Double val2){
         return val1 + val2;
     }
+
+    /**
+     * Метод вычитания второго числа из первого
+     * @param val1
+     * @param val2
+     * @return
+     */
     public Double minus( Double val1, Double val2){
         return val1 - val2;
     }
+
+    /**
+     * Метод умножения двух чисел
+     * @param val1
+     * @param val2
+     * @return
+     */
     public Double multiply( Double val1, Double val2){
         return val1 * val2;
     }
 
+    /**
+     * Метод деления первого числа на второе
+     * @param val1
+     * @param val2
+     * @return
+     */
     public Double div( Double val1, Double val2){
         return val1 / val2;
     }
